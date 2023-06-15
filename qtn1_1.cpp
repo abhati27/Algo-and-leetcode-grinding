4a681b70bf6df19dadf1a04e6841d43839ba4eee
Anmol Bhatia <60200047+abhati27@users.noreply.github.com>
2023-06-14 23:47:21 -0400

    add qtn 1.1 from ctci
    this solution checks if a string is unique... like basically for an ascii character set

---
+++ qtn1_1.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <stdbool.h>
+#include <string>
+using namespace std;
+
+bool isUnique(string input) {
+  // ask what is the type of charater set  ascii or unicode?
+  // lets take ascii
+  bool arr[128] = {false};
+  for (int i = 0; i < input.size(); i++) {
+    if (arr[input[i]] == false) {
+      arr[input[i]] = true;
+    } else {
+        return false;
+    }
+  }
+return true;
+}
+
+
+int main() {
+  cout << "Check if letters are unique" << endl;
+  cout << "Enter the string: ";
+  string input;
+  cin >> input;
+  int res = isUnique(input);
+  cout << ((res == 1) ? "All characters are unique":"Not all characters are unique")<<endl;
+  cout << "end of qtn 1.1"<< endl << "Teehee...";  
+}
